ec5f81e14ba33b21518f3318445f86af28f9348d
Mark Makuno <makunomark@gmail.com>
2020-01-30 14:32:26 +0300

    Update author

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Tailwind Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@makunomark`,
   },
   plugins: [
     `gatsby-plugin-postcss`,
